b3f4cb03bb016f6b97f2bd7784b967373d42b1df
dagr27 <00118717@uca.edu.sv>
2018-10-26 19:36:10 -0600

    Subiendo Todolist

---
+++ TodoList/js/script.js
@@ -0,0 +1,67 @@
+window.onload = init;
+   
+ function init() {
+     let todoList = {
+         listHTML: document.getElementById("todoList"),
+         listTask: [],
+         add(task, priority = false) {
+             let element = document.createElement("li");
+             element.innerText = task;
+             /*element.addEventListener("click", () => {
+                let parent = element.parentNode;
+                if(parent){
+                    parent.removeChild(element);
+                }
+             });*/
+             element.addEventListener("click", function(){
+                console.log(this);
+                let parent = this.parentNode;
+                if(parent){
+                    parent.removeChild(this);
+                }
+             });
+            // Añadir un boton para marcar de finalizado
+            // Elmine de la lista
+
+             if (priority) {
+                 this.listTask.unshift({
+                     element,
+                     task
+                 });
+                 this.listHTML.insertBefore(element, this.listHTML.childNodes[0]);
+             } else {
+                 this.listTask.push({
+                     element,
+                     task
+                 });
+                 this.listHTML.appendChild(element);
+             }
+         }
+     }
+
+     let form = document.managerTask;
+     form.addEventListener("submit", (evt) => {
+         evt.preventDefault();
+         let task = form.task.value;
+
+         let validTask = /.{2,}/;
+         if (!validTask.test(task)) {
+             console.log("Ingrese una descripcion clara");
+             return false;
+         }
+
+         todoList.add(task, form.important.checked);
+     });
+	 
+     document.getElementById("checkList").addEventListener("click", (evt) => {
+        evt.preventDefault();
+        let i = document.getElementById("todoList");
+        i.childNodes[0].style.textDecoration = "line-through";
+     });
+	 document.getElementById("delete").addEventListener("click", (evt) => {
+        evt.preventDefault();
+        let pos = document.getElementById("todoList");
+        pos.removeChild(pos.childNodes[0]);
+     });
+
+}
